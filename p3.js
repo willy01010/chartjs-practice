88b7b1cf643a08fb4dcdcb5890b63725c61e5926
willy01010 <b0921217@cgu.edu.tw>
2023-02-07 10:42:46 +0800

    p3 ok

--- p3.js
+++ p3.js
@@ -1,16 +1,6 @@
 const ctx = document.getElementById('myChart');
-
-
-let t = '';
-
-const data = [
-  // { year: 2010, count: 10 },
-  // { year: 2011, count: 20 },
-  // { year: 2012, count: 15 },
-  // { year: 2013, count: 25 },
-];
-
-
+let initdataset = '';
+let mychart;
 
 
 var current = "";
@@ -31,22 +21,34 @@ db.ref('on_off').on('value', (data) => {//取得目前車輛狀態，供GeoJson�
 var speed = [], time = [];
 function getcargeojson(num = -1) {
   if (num == -1) {
-    db.ref('CarGeoJson/_' + current).on('value', (data) => {//將GeoJson資料dowmload
+    db.ref('CarGeoJson/_' + current).once('value', (data) => {//將GeoJson資料dowmload
       // craline.setMap(null);
       speed = [], time = [];
       time = data.val().features[0].properties.times;
       speed = data.val().features[0].properties.speed;
+      init(time,'speed',speed);
 
-      for (var i = 0; i < time.length; i++) {
-        addBigData(time[i], speed[i]);
-      }
-
-      cc.update();
 
 
       addDataset('battery', data.val().features[0].properties.battery, 'rgb(255, 99, 132)', 'rgb(255, 153, 153)','y1');
     });
-  } 
+  } else{
+
+    db.ref('CarGeoJson/_' + num).once('value', (data) => {//將GeoJson資料dowmload
+      // craline.setMap(null);
+      clearChart();
+
+      speed = [], time = [];
+      time = data.val().features[0].properties.times;
+      speed = data.val().features[0].properties.speed;
+      init(time,'speed',speed);
+
+
+
+      addDataset('battery', data.val().features[0].properties.battery, 'rgb(255, 99, 132)', 'rgb(255, 153, 153)','y1');
+    });
+
+  }
 
 }
 
@@ -55,80 +57,171 @@ function getcargeojson(num = -1) {
 
 
 
-t = {
-  type: 'line',
-  data: {
-    labels: '',
-    datasets: [
-      {
-        label: 'speed',
-        data: []
-      }
-    ]
-  },
-  options: {
-    scales: {
-      x: {
-        display: true,
-        title: {
-          display: true,
-          text: 'Time',
-          color: '#911',
-          font: {
-            family: 'Comic Sans MS',
-            size: 20,
-            weight: 'bold',
-            lineHeight: 1.2,
-          },
-          padding: { top: 20, left: 0, right: 0, bottom: 0 }
-        }
+function init(xlabel=[],dsName='',value=[]){
+
+  if(dsName!=''){
+
+    initdataset = {
+      type: 'line',
+      data: {
+        labels: xlabel,
+        datasets: [
+          {
+            label: dsName,
+            data: value
+          }
+        ]
       },
-      y: {
-        display: true,
-        position: 'left',
-        title: {
-          display: true,
-          text: 'Km/hr',
-          color: '#191',
-          font: {
-            family: 'Times',
-            size: 20,
-            style: 'normal',
-            lineHeight: 1.2
+      options: {
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Time',
+              color: '#911',
+              font: {
+                family: 'Comic Sans MS',
+                size: 20,
+                weight: 'bold',
+                lineHeight: 1.2,
+              },
+              padding: { top: 20, left: 0, right: 0, bottom: 0 }
+            }
           },
-          padding: { top: 30, left: 0, right: 0, bottom: 0 }
+          y: {
+            display: true,
+            position: 'left',
+            title: {
+              display: true,
+              text: 'Km/hr',
+              color: '#191',
+              font: {
+                family: 'Times',
+                size: 20,
+                style: 'normal',
+                lineHeight: 1.2
+              },
+              padding: { top: 30, left: 0, right: 0, bottom: 0 }
+            }
+          },
+    
+          y1: {
+            display: true,
+            position: 'right',
+            max:100,
+            min:0,
+            title: {
+              display: true,
+              text: '%',
+              color: '#191',
+              font: {
+                family: 'Times',
+                size: 20,
+                style: 'normal',
+                lineHeight: 1.2
+              },
+              padding: { top: 30, left: 0, right: 0, bottom: 0 }
+            }
+          }
+    
         }
+    
       },
+    };
+    
+     mychart = new Chart(ctx, initdataset);
+    
+    
+    console.log(mychart.data);
+    
 
-      y1: {
-        display: true,
-        position: 'right',
-        max:100,
-        min:0,
-        title: {
-          display: true,
-          text: '%',
-          color: '#191',
-          font: {
-            family: 'Times',
-            size: 20,
-            style: 'normal',
-            lineHeight: 1.2
+
+
+    
+  }else{
+
+    initdataset = {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: '',
+            data: []
+          }
+        ]
+      },
+      options: {
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Time',
+              color: '#911',
+              font: {
+                family: 'Comic Sans MS',
+                size: 20,
+                weight: 'bold',
+                lineHeight: 1.2,
+              },
+              padding: { top: 20, left: 0, right: 0, bottom: 0 }
+            }
           },
-          padding: { top: 30, left: 0, right: 0, bottom: 0 }
+          y: {
+            display: true,
+            position: 'left',
+            title: {
+              display: true,
+              text: 'Km/hr',
+              color: '#191',
+              font: {
+                family: 'Times',
+                size: 20,
+                style: 'normal',
+                lineHeight: 1.2
+              },
+              padding: { top: 30, left: 0, right: 0, bottom: 0 }
+            }
+          },
+    
+          y1: {
+            display: true,
+            position: 'right',
+            max:100,
+            min:0,
+            title: {
+              display: true,
+              text: '%',
+              color: '#191',
+              font: {
+                family: 'Times',
+                size: 20,
+                style: 'normal',
+                lineHeight: 1.2
+              },
+              padding: { top: 30, left: 0, right: 0, bottom: 0 }
+            }
+          }
+    
         }
-      }
+    
+      },
+    };
+    
+     mychart = new Chart(ctx, initdataset);
+    
+    
+    console.log('create empty chart. please use addData/Dataset func to add Data');
+    
 
-    }
-
-  },
-};
-
-let cc = new Chart(ctx, t);
 
 
-console.log(cc.data);
+    
+  }
 
+}
 
 
 
@@ -137,8 +230,14 @@ console.log(cc.data);
 
 
 //---------------------------
+function clearChart(){
+  mychart.destroy();
+  
+}
+
+
 function addData(xlabel, num) {
-  const data = cc.data;
+  const data = mychart.data;
   if (data.datasets.length > 0) {
     let label = data.labels;
 
@@ -148,28 +247,14 @@ function addData(xlabel, num) {
     }
     label.push(xlabel);
 
-    cc.update();
+    mychart.update();
   }
 }
 
-function addBigData(xlabel, num){
-  const data = cc.data;
-  if (data.datasets.length > 0) {
-    let label = data.labels;
 
-    for (let index = 0; index < data.datasets.length; ++index) {
-      data.datasets[index].data.push(num);
-      console.log('data.datasets[index].data.push(num)', index, 'num:', num)
-    }
-    label.push(xlabel);
-
-    
-  }
-}
 
 function addDataset(dsName, value, dsColor, dsBgColor,yAxisID) {
-  const data = cc.data;
-  // const dsColor = 'rgb(255, 99, 132)';
+
 
   const newDataset = {
     label: dsName,
@@ -179,30 +264,27 @@ function addDataset(dsName, value, dsColor, dsBgColor,yAxisID) {
     yAxisID: yAxisID,
   };
 
-  cc.data.datasets.push(newDataset);
-  cc.update();
+  mychart.data.datasets.push(newDataset);
+  mychart.update();
 
-  console.log('新增', cc.data);
+  console.log('新增', mychart.data);
 
 }
 
 
 
 function removeData() {
-  cc.data.labels.splice(-1, 1); // remove the label first
+  mychart.data.labels.splice(-1, 1); // remove the label first
 
-  cc.data.datasets.forEach(dataset => {
+  mychart.data.datasets.forEach(dataset => {
     dataset.data.pop();
   });
 
-  cc.update();
+  mychart.update();
 }
 
 function removeDataset() {
-  cc.data.datasets.pop();
-  // for (var i = 0; i < cc.data.labels.length; i++) {
-  //   cc.data.labels.pop();
-  // }
+  mychart.data.datasets.pop();
 
-  cc.update();
+  mychart.update();
 }
